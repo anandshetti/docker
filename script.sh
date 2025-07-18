b3eda197cb6021c8b3cd9e91e33719cd036c2018
Anand_03 <131592969+anandshetti@users.noreply.github.com>
2025-07-18 11:27:54 +0530

    Update script.sh
    deleted few code

--- script.sh
+++ script.sh
@@ -5,16 +5,7 @@ sour="/home/anand/DB_backup"
 desti="/home/ram/DB_backup"
 
 
-if [!f >= o ]then
-echo " copying the files...!"
-fi
-
-chown -R anand:anand /Db_backup
 
 
-exit the script 
-
-echo " new line" >> file.txt 
 
 
-today edit 
